85460e07014aa4542e3a4bd638840cd83957b57e
zainab-gilani <zainab.gilani@yahoo.com>
2025-04-28 15:01:40 +0400

    Added index function

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def detail(request, question_id):
@@ -14,3 +15,6 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
