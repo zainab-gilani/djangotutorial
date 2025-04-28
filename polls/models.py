549d7b8355501c72f34997d82995497063586f35
zainab-gilani <zainab.gilani@yahoo.com>
2025-04-28 14:05:45 +0400

    Fixed Question

--- polls/models.py
+++ polls/models.py
@@ -7,9 +7,14 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     #enddef
 #endclass
 
