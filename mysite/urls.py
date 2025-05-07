93b894dba20de043977c9cb1abd2c787147f63d9
zainab-gilani <zainab.gilani@yahoo.com>
2025-05-07 14:48:55 +0400

    Fixed bugs in paths

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,18 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from polls import views
-
-
+from polls.views import IndexView, DetailView, ResultsView
 
 urlpatterns = [
     # ex: /polls/
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
-    path("", views.index, name="index"),
+    path("", IndexView.as_view(), name="index"),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/", DetailView.as_view(), name="detail"),
     # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/results/", ResultsView.as_view(), name="results"),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
