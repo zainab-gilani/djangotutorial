460824510a8b3119363111ae00f74c4a9a9ed268
zainab-gilani <zainab.gilani@yahoo.com>
2025-05-08 07:59:24 +0400

    Added new views

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.test import TestCase
 from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
@@ -6,7 +7,7 @@ from django.views import generic
 from django.utils import timezone
 
 from .models import Choice, Question
-
+from .tests import create_question
 
 
 # Create your views here.
@@ -22,6 +23,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -59,3 +66,25 @@ def get_queryset(self):
     return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
         :5
     ]
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
