6ec53b2328dbf4627b01330c5563702703ebfa14
zainab-gilani <zainab.gilani@yahoo.com>
2025-05-08 11:46:23 +0400

    Improved display

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 from django.utils import timezone
 
+from django.contrib import admin
+
 # Create your models here.
 
 class Question(models.Model):
@@ -11,6 +13,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
 
     def was_published_recently(self):
         now = timezone.now()
@@ -27,6 +35,7 @@ class Choice(models.Model):
         return self.choice_text
     #enddef
     def was_published_recently(self):
-        return self.question.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.question.pub_date <= now
     #enddef
 #endclass
