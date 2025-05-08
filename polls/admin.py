25eb76b37e5bbec40b6e7c63ab274faad787195e
zainab-gilani <zainab.gilani@yahoo.com>
2025-05-08 11:45:53 +0400

    Added list_display and list_filter

--- polls/admin.py
+++ polls/admin.py
@@ -14,7 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
-    list_display = ["question_text", "pub_date", "was_published_recently"]
-
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
 
 admin.site.register(Question, QuestionAdmin)
